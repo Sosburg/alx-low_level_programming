67d104d07904145cc1890fbe2ea7408fea07627f
Sosburg <oloratojobeta@gmail.com>
2024-01-26 03:55:14 -0800

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 
 /**
-*  * main - Entry point
-*   * @argc: The number of command-line arguments
-*    * @argv: An array of command-line argument strings
-*     *
-*      * Return: 0 on success, 1 on error
+*main - Entry point
+*@argc: The number of command-line arguments
+*@argv: An array of command-line argument strings
+*Return: 0 on success, 1 on error
 */
 int main(int argc, char *argv[])
 {
+int num1, num2, result;
+
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
 
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-int result = num1 * num2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 *num2;
 
 printf("%d\n", result);
 
