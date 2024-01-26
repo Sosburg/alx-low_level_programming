5aae497378923e75584e0161f6c7d9a01627c145
Sosburg <oloratojobeta@gmail.com>
2024-01-26 02:38:01 -0800

    prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*  * main - Entry point
+*   * @argc: The number of command-line arguments
+*    * @argv: An array of command-line argument strings
+*     *
+*      * Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}
