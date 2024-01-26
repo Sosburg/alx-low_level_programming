bb36c123ac12c31be65796719387d176fa3c725b
Sosburg <oloratojobeta@gmail.com>
2024-01-26 01:56:21 -0800

    prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  * print_program_name - Prints the name of the program
+ *   * @name: Name of the program
+ *    */
+void print_program_name(char *name)
+{
+	    /* Print the name of the program followed by a new line */
+	    while (*name != '\0')
+		        {
+				        _putchar(*name);
+					        name++;
+						    }
+	        _putchar('\n');
+}
