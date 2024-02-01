0eebca6a3bf5f3c5d2a39e93cb093fc03363b2f0
Sosburg <oloratojobeta@gmail.com>
2024-02-01 11:45:19 -0800

    Fixed errors

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,13 @@
 #include <stddef.h>
 
 /**
-*  * init_dog - initializes a variable of type struct dog
-*   * @d: pointer to struct dog
-*    * @name: name of the dog
-*     * @age: age of the dog
-*      * @owner: owner of the dog
+*init_dog - initializes a variable of type struct dog
+*@d: pointer to struct dog
+*@name: name of the dog
+*@age: age of the dog
+*@owner: owner of the dog
 */
-void init_dog(dog_t *d, char *name, float age, char *owner)
+void init_dog(dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
 {
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-*  * struct dog - a structure representing information about a dog
-*   * @name: the name of the dog
-*    * @age: the age of the dog
-*     * @owner: the owner of the dog
+*struct dog - a structure representing information about a dog
+*@name: the name of the dog
+*@age: the age of the dog
+*@owner: the owner of the dog
 */
 struct dog
 {
@@ -15,6 +15,6 @@ char *owner;
 
 typedef struct dog dog_t;
 
-void init_dog(dog_t *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
