28cdb7a7bfd62199ae941eda899c67420c18ea51
Sosburg <oloratojobeta@gmail.com>
2024-02-13 01:20:28 -0800

    betty main

--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -3,20 +3,21 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+* @ac: Number of command-line arguments
+* @av: Array of command-line argument strings
+* Return: Always 0.
+*/
 int main(int ac, char **av)
 {
-    int res;
+int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+if (ac != 3)
+{
+dprintf(2, "Usage: %s filename text\n", av[0]);
+exit(1);
+}
+res = append_text_to_file(av[1], av[2]);
+printf("-> %i)\n", res);
+return (0);
 }
