762983f932285f0619e47be5a25dc035a3a6d8dc
Sosburg <oloratojobeta@gmail.com>
2024-02-13 00:57:41 -0800

    betty

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* create_file - Creates a file and writes text content to it.
+* @filename: The name of the file to create.
+* @text_content: The NULL-terminated string to write to the file.
+* Return: 1 on success, -1 on failure.
+*/
+int create_file(const char *filename, char *text_content)
+{
+int file_descriptor, write_result;
+mode_t file_permissions = S_IRUSR | S_IWUSR; /* rw------- */
+
+if (filename == NULL)
+return (-1);
+
+file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
+if (file_descriptor == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+write_result = write(file_descriptor, text_content, strlen(text_content));
+if (write_result == -1)
+{
+close(file_descriptor);
+return (-1);
+}
+}
+
+close(file_descriptor);
+return (1);
+}
