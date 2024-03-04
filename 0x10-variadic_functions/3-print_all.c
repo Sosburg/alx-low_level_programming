272a5e00d4c867c79e67cdad404a14222975e0b8
Sosburg <oloratojobeta@gmail.com>
2024-03-04 11:03:11 -0800

    still errors

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,41 +10,41 @@ void print_all(const char * const format, ...)
 va_list args;
 unsigned int i = 0;
 char *str;
-char separator = ', ';
+char separator = 0;
 
 va_start(args, format);
 
 while (format && format[i])
 {
+if (separator)
+printf(", ");
+
 switch (format[i])
 {
 case 'c':
 printf("%c", va_arg(args, int));
+separator = 1;
 break;
 case 'i':
 printf("%d", va_arg(args, int));
+separator = 1;
 break;
 case 'f':
 printf("%f", va_arg(args, double));
+separator = 1;
 break;
 case 's':
 str = va_arg(args, char *);
 if (str == NULL)
 str = "(nil)";
 printf("%s", str);
+separator = 1;
 break;
 default:
 separator = 0;
 break;
 }
-
-if (separator && format[i + 1])
-printf("%c ", separator);
-
 i++;
 }
-
 va_end(args);
-
-printf("\n");
-}
+printf("\n"); }
