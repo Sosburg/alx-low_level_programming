e81e7c3ea9cb5dcd5c12febd12e603fefcea8a52
Sosburg <oloratojobeta@gmail.com>
2024-03-04 12:09:21 -0800

    errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,13 +16,13 @@ dog_t *new_dog;
 
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
-return (NULL);
+return NULL;
 
 new_dog->name = strdup(name);
 if (new_dog->name == NULL)
 {
 free(new_dog);
-return (NULL);
+return NULL;
 }
 
 new_dog->owner = strdup(owner);
@@ -30,11 +30,9 @@ if (new_dog->owner == NULL)
 {
 free(new_dog->name);
 free(new_dog);
-return (NULL);
+return NULL;
 }
 
-new_dog->age = age;
-
-return (new_dog);
+return new_dog;
 }
 
